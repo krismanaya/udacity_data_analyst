819a04ba96ae3a8879201988d96432a0138877f1
krismanaya <krismanaya@gmail.com>
2017-04-07 23:21:34 -0700

    fix db

--- project_3/osm_src/data_wrangling_schema.sql
+++ project_3/osm_src/data_wrangling_schema.sql
@@ -44,25 +44,4 @@ CREATE TABLE ways_nodes (
 
 
 
- SELECT tags.value, COUNT(*) as count 
-FROM (SELECT * FROM nodes_tags UNION ALL 
-      SELECT * FROM ways_tags) tags
-WHERE tags.key LIKE '%city'
-GROUP BY tags.value
-ORDER BY count DESC;
 
-SELECT nodes_tags.value, COUNT(*) as num
-FROM nodes_tags 
-    JOIN (SELECT DISTINCT(id) FROM nodes_tags WHERE value='bar') i
-    ON nodes_tags.id=i.id
-WHERE nodes_tags.key == 'name' 
-GROUP BY nodes_tags.value
-ORDER BY num DESC;
-
-SELECT nodes_tags.value, COUNT(*) as num
-FROM nodes_tags 
-    JOIN (SELECT DISTINCT(id) FROM nodes_tags WHERE value='fitness_centre') i
-    ON nodes_tags.id=i.id
-WHERE nodes_tags.key == 'name' 
-GROUP BY nodes_tags.value
-ORDER BY num DESC;
