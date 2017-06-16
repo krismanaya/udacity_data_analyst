3f5b2fd06fea6bd86feda7709ce21eb18dde9cb8
Kris Anaya <krismanaya@gmail.com>
2017-06-15 22:26:30 -0700

    make a decision tree

--- project_5/decision_tree/dt_author_id.py
+++ project_5/decision_tree/dt_author_id.py
@@ -13,7 +13,6 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -22,10 +21,48 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+########################################################
+def DTAccuracy(features_test, labels_test): 
+    from sklearn.metrics import accuracy_score #accuracy score
+
+
+
+    t0 = time() # intial run time
+    clf = classify(features_train,labels_train) #create classifier
+    print "training time:", round(time() - t0, 3), "s"
+
+    t1 = time() #create time 
+    pred = clf.predict(features_test) #create prediction
+    print "predictive time:", round(time() - t1, 3), "s"
+
+    accuracy = accuracy_score(labels_test, pred) # accuracy test
+
+    return {"You accuracy is": accuracy}
+
+
+def classify(features_train, labels_train): 
+    from sklearn import tree # import dt
+
+    clf = tree.DecisionTreeClassifier() # create classifier
+    clf = clf.fit(features_train, labels_train) # fit training set 
+    
+    return clf
+
 #########################################################
-### your code goes here ###
+
+def main():
+    
+    clf = classify(features_train, labels_train)
+    accuracy = DTAccuracy(features_test,labels_test)
+
+    print clf 
+    print accuracy
+    
+##### run code ##### 
+
+if __name__ == '__main__':
+    main() #show output. 
 
 
-#########################################################
-
+########################################################
 
