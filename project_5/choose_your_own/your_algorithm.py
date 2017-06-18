f790343afe5eacf5757cb1f750b7a350959ea5b3
Kris Anaya <krismanaya@gmail.com>
2017-06-18 16:47:18 -0700

    create a knn

--- project_5/choose_your_own/your_algorithm.py
+++ project_5/choose_your_own/your_algorithm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+from time import time
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
@@ -17,14 +18,14 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
@@ -32,13 +33,54 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 
 
+################## KNN ##################
+
+def classify(features_train, labels_train): 
+    from sklearn.neighbors import KNeighborsClassifier as knnc
+
+    clf = knnc(n_neighbors = 3, leaf_size = 50,  
+                weights = "distance", 
+                algorithm = "ball_tree", 
+                metric = "chebyshev") # classifier 
+    clf = clf.fit(features_train, labels_train)
+
+    return clf
+
+##################KNN-ACCURACY#############################
+def KNNAccuracy(features_test, labels_test): 
+    from sklearn.metrics import accuracy_score #accuracy score
+
+    t0 = time() # intial run time
+    clf = classify(features_train,labels_train) #create classifier
+    print "training time:", round(time() - t0, 3), "s"
+
+    t1 = time() #create time 
+    pred = clf.predict(features_test) #create prediction
+    print "predictive time:", round(time() - t1, 3), "s"
+
+    accuracy = accuracy_score(labels_test, pred) # accuracy test
+
+    return {"You accuracy is": accuracy}
 
 
 
+################## Main ##################
+def main():
+
+    clf = classify(features_train, labels_train) 
+    accuracy = KNNAccuracy(features_test,labels_test)
+    print clf
+    print accuracy
+
+    try: 
+        prettyPicture(clf, features_test, labels_test)
+        plt.show()
+    except NameError: 
+        pass
+
+##### run code ##### 
+
+if __name__ == '__main__':
+    main()
 
 
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
