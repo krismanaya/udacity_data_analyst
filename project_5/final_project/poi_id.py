0ad7bb6a0f4ce581bef1d26830a950a3723a88ec
Kris Anaya <krismanaya@gmail.com>
2017-07-19 18:16:43 -0700

    dump classifier

--- project_5/final_project/poi_id.py
+++ project_5/final_project/poi_id.py
@@ -4,54 +4,104 @@ import sys
 import pickle
 import pprint
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np 
+import pprint
+import warnings
+warnings.filterwarnings('ignore')
+from time import time 
+
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+
+### import classifiers, splitters, metrics
+
+from sklearn.svm import SVC                                ### Support vector machine 
+from sklearn import tree                                   ### Decision Tree 
+from sklearn.naive_bayes import GaussianNB                 ### Naive Bayes
+from sklearn.neighbors import KNeighborsClassifier as knnc ### KNeighbors
+
+from sklearn import cross_validation                ### CV
+from sklearn.metrics import accuracy_score          ### Accuracy
+from sklearn.metrics import confusion_matrix        ### Confusion 
+from sklearn.metrics import precision_score         ### Precision 
+from sklearn.metrics import recall_score            ### Recall Score
+from sklearn.metrics import classification_report   ### report card
+from sklearn.grid_search import GridSearchCV        ### GridSearch           
+from sklearn.ensemble import ExtraTreesClassifier   ### Extra tree
+from sklearn.linear_model import RandomizedLasso    ### good for feature selection
+from sklearn.ensemble import RandomForestRegressor  ### goot for non-linear 
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
 
 
-features_list = ['poi','salary','bonus'] # You will need to use more features
+features_list = ['poi','salary', 'deferral_payments', 'total_payments', 
+                 'loan_advances', 'bonus', 'restricted_stock_deferred', 
+                 'deferred_income', 'total_stock_value', 'expenses', 
+                 'exercised_stock_options', 'other', 
+                 'long_term_incentive', 'restricted_stock', 
+                 'director_fees','to_messages',  
+                 'from_poi_to_this_person', 'from_messages', 
+                 'from_this_person_to_poi', 'shared_receipt_with_poi', 
+                 'salary_bonus_ratio', 'exercised_ratio', 'total_sp_ratio', 
+                 'restricted_ratio','to_poi_ratio', 'from_poi_ratio']
+
+# You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
+### dump data_dict into pandas df for EDA. 
+
+### create df from list of values, create a series of employees and index
+
+df = pd.DataFrame.from_records(list(data_dict.values()))
+employees = pd.Series(list(data_dict.keys()))
+df.set_index(employees, inplace = True)
+
+### Replace "NaN" values for Zero.
+df.replace("NaN", 0, inplace= True)
+
 ### Task 2: Remove outliers
-### Decided to only remove total as an outlier first
-### With selected features
-### Output now selects "SKILLING JEFFREY K, FREVERT MARK, A", which should
-### not be removed.
 
-features = ['salary','bonus'] 
-data_dict.pop("TOTAL") 
+### Drop Total and TAITP 
 
-for person in data_dict: 
-    salary = data_dict[person][features[0]]
-    bonus = data_dict[person][features[1]]
-    if salary != 'NaN' and bonus != 'NaN' and salary >= 1000000 and bonus > 1000000:
-        print person
+df = df.drop(["TOTAL", 'THE TRAVEL AGENCY IN THE PARK'])
 
-data = featureFormat(data_dict, features)
 
-for point in data:
-    salary = point[0]
-    bonus = point[1]
-#   plt.scatter(salary,bonus)
-
-print "Size of data {0}", format(data.shape)
-salary = []; bonus = []
-salary = data[:,0] 
-bonus = data[:,1]
-plt.scatter(salary,bonus)
-plt.xlabel('salary')
-plt.ylabel('bonus')
-plt.show()
 ### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
+
+### Replace 0 to np.nan 
+
+df.replace(0, np.nan, inplace= True)
+
+### financial features 
+df['salary_bonus_ratio'] = df['salary'] / df['bonus']
+
+df['exercised_ratio']    = df['exercised_stock_options'] / df['total_stock_value']
+
+df['total_sp_ratio']     = df['total_payments'] / df['total_stock_value']
+
+df['restricted_ratio']   = df['restricted_stock'] / df['total_stock_value']
+
+### email features 
+df['to_poi_ratio']   = df['from_this_person_to_poi'] / df['from_messages']
+
+df['from_poi_ratio'] = df["from_poi_to_this_person"] / df['to_messages']
+
+
+### Step 1 - replace np.nan --> "NaN" 
+df.replace(np.nan, "NaN", inplace = True)
+
+### Step 2 - Create dictionary 
+data_dict = df.to_dict('index')
+
+### Step 3 - Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 
@@ -75,15 +125,24 @@ clf = GaussianNB()
 ### function. Because of the small size of the dataset, the script uses
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+### Please see Ipython notebook for details on how I tuned.
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+### create a test an train cv using 30% test_size
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, 
+                                                                                             labels, 
+                                                                                             test_size = 0.3, 
+                                                                                             random_state = 42)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
+
+clf = tree.DecisionTreeClassifier(class_weight='balanced', criterion='entropy',
+            max_depth=6, max_features=None, max_leaf_nodes=None,
+            min_samples_leaf=2, min_samples_split=1,
+            min_weight_fraction_leaf=0.0, presort=False, random_state=3,
+            splitter='best')
 
 dump_classifier_and_data(clf, my_dataset, features_list)
